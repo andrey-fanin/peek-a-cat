262628944ed5f693e7cba65fa43a61a4b61f3456
Andrey Fanin <andr.fanin16@gmail.com>
2023-09-26 00:57:52 +0500

    refactor card component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
-<script setup></script>
+<script setup>
+import Card from '@/components/Card.vue'
+
+const cardList = Array.from({ length: 16 }, (x, i) => i++)
+</script>
 
 <template>
-  <h1>game!</h1>
+  <div class="wrapper">
+    <h1>game!</h1>
+    <section class="game-board">
+      <Card v-for="card in cardList" :key="card" :order="card" />
+    </section>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    margin-inline: auto;
+  }
+}
+
+.game-board {
+  width: calc(102px * 4 + 10px * 3);
+  gap: 10px;
+  margin-inline: auto;
+  display: flex;
+  flex: 1 1 25%;
+  flex-wrap: wrap;
+}
+</style>
