d427ee7cb7a3063ae66f09d148d331b358e262b5
Andrey Fanin <andr.fanin16@gmail.com>
2023-10-05 00:43:44 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { computed, ref, watch } from 'vue'
 import { shuffle } from '@/utils/shuffleArray'
 
 const cardList = ref([])
+console.log(cardList)
 
 const cardItems = [1, 2, 3, 4, 5, 6, 7, 8]
 
